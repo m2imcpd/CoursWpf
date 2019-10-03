4bb09a3b9ef2a46e65199863f9d07337da07c599
ihab <m2imcdp@gmail.com>
2019-10-03 15:44:57 +0200

    Cours WPF Ouverture des fenetres

---
+++ CoursWPF/CoursWPF/Info.xaml.cs
@@ -0,0 +1,37 @@
+﻿using CoursWPF.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoursWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour Info.xaml
+    /// </summary>
+    public partial class Info : Window
+    {
+        public Info()
+        {
+            InitializeComponent();
+        }
+
+        public Info(string info) : this()
+        {
+            tb1.Text = info;
+        }
+        public Info(Personne p) : this()
+        {
+            tb1.Text = p.Nom + " "+p.Prenom;
+        }
+    }
+}
---
+++ CoursWPF/CoursWPF/StackPanel.xaml.cs
@@ -0,0 +1,45 @@
+﻿using CoursWPF.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoursWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour StackPanel.xaml
+    /// </summary>
+    public partial class StackPanel : Window
+    {
+        public StackPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Taquin tWindow = new Taquin();
+            tWindow.Show();
+            tWindow.Closed += (s, el) =>
+             {
+                 MessageBox.Show("Taquin fermé");
+             };
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Info iWindow = new Info(new Personne { Nom = t1.Text, Prenom = t2.Text });
+            iWindow.Show();
+        }
+    }
+}
