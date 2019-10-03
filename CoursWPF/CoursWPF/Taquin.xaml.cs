4acaef69266c732b445720b85871d852be6519d0
ihab <m2imcdp@gmail.com>
2019-10-03 10:57:39 +0200

    Correction Taquin

--- CoursWPF/CoursWPF/Taquin.xaml.cs
+++ CoursWPF/CoursWPF/Taquin.xaml.cs
@@ -55,6 +55,20 @@ namespace CoursWPF
 
         private void CreateButton(object sender, RoutedEventArgs e)
         {
+            //supprimer les anciens boutons
+            //List<UIElement> listeASupprimer = new List<UIElement>();
+            //foreach(UIElement element in maGrille.Children)
+            //{
+            //    if(Grid.GetRow(element) > 0)
+            //    {
+            //        listeASupprimer.Add(element);
+            //    }
+            //}
+            //listeASupprimer.ForEach(el =>
+            //{
+            //    maGrille.Children.Remove(el);
+            //});
+            maGrille.Children.Cast<UIElement>().Where(x=> Grid.GetRow(x) > 0).ToList().ForEach(x=>maGrille.Children.Remove(x));
             Shuffle(tabChar);
             int k = 0;
             for(int i= 1; i < 4; i++)
@@ -64,6 +78,7 @@ namespace CoursWPF
                     if(k < 8)
                     {
                         Button b = new Button { Content = tabChar[k] };
+                        b.Click += Click_bouton;
                         Grid.SetRow(b, i);
                         Grid.SetColumn(b, j);
                         maGrille.Children.Add(b);
@@ -84,6 +99,76 @@ namespace CoursWPF
                 tab[aleatoire] = tab[i];
                 tab[i] = tmp;
             }
+        }
+
+        private void Click_bouton(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button)
+            {
+                Button b = (sender as Button);
+                int x = Grid.GetRow(b);
+                int y = Grid.GetColumn(b);
+                if(Test_Deplacement(x+1, y) && x < 3)
+                {
+                    Grid.SetRow(b, x + 1);
+                }
+                else if(Test_Deplacement(x-1, y) && x > 1)
+                {
+                    Grid.SetRow(b, x - 1);
+                }
+                else if(Test_Deplacement(x, y+1) && y < 2)
+                {
+                    Grid.SetColumn(b, y + 1);
+                }
+                else if (Test_Deplacement(x, y - 1) && y > 0)
+                {
+                    Grid.SetColumn(b, y - 1);
+                }
+                labelCpt.Content = Convert.ToInt32(labelCpt.Content.ToString()) + 1;
+                if(Win())
+                {
+                    MessageBox.Show("Bravo");
+                }
+            }
+        }
+
+        private bool Test_Deplacement(int x, int y)
+        {
+            //Return true si elementTrouve est null
+            //UIElement elementTrouve = null;
+            //foreach(UIElement element in maGrille.Children)
+            //{
+            //    if(Grid.GetRow(element) == x && Grid.GetColumn(element) == y)
+            //    {
+            //        elementTrouve = element;
+            //        break;
+            //    }
+            //}
+            //return elementTrouve == null;
+            //<=>
+            return maGrille.Children.Cast<UIElement>().FirstOrDefault(e => (Grid.GetColumn(e) == y && Grid.GetRow(e) == x)) == null;
+        }
+
+        private bool Win()
+        {
+            string chaineW = "ABCDEFGH#";
+            string chaine = "";
+            for(int i=1; i < 4; i++)
+            {
+                for(int j=0; j < 3; j++)
+                {
+                    UIElement b = maGrille.Children.Cast<UIElement>().FirstOrDefault(e => (Grid.GetColumn(e) == j && Grid.GetRow(e) == i));
+                    if(b == null)
+                    {
+                        chaine += "#";
+                    }
+                    else
+                    {
+                        chaine += (b as Button).Content.ToString();
+                    }
+                }
+            }
+            return chaine == chaineW;
         }
     }
 }
