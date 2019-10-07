a6700f43878b0cab161ef448b56d3b8f0cf518f4
ihab <m2imcdp@gmail.com>
2019-10-07 16:49:52 +0200

    correction banque avec InotifyPropertychanged

--- CoursWPF/BanqueWpf/Classes/Compte.cs
+++ CoursWPF/BanqueWpf/Classes/Compte.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BanqueWpf.Classes
 {
-    public class Compte
+    public class Compte : INotifyPropertyChanged
     {
         private int id;
         private string numero;
@@ -16,8 +18,24 @@ namespace BanqueWpf.Classes
 
         public int Id { get => id; set => id = value; }
         public string Numero { get => numero; set => numero = value; }
-        public decimal Solde { get => solde; set => solde = value; }
+        public decimal Solde
+        {
+            get => solde;
+            set
+            {
+                solde = value;
+                NotifyChange();
+            }
+        }
         public Client Client { get => client; set => client = value; }
         public List<Operation> ListeOperations { get => listeOperations; set => listeOperations = value; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyChange([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
