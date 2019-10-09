a773c3dd1d47de179fd373da1b3f418620a0e994
ihab <m2imcdp@gmail.com>
2019-10-09 09:46:46 +0200

    correction avec data.sql

---
+++ CoursWPF/CorrectionAnnuaire/data.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[contact_wpf] (
+    [Id]        INT          IDENTITY (1, 1) NOT NULL,
+    [nom]       VARCHAR (50) NOT NULL,
+    [prenom]    VARCHAR (50) NOT NULL,
+    [telephone] VARCHAR (10) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[email_wpf] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [mail]      VARCHAR (MAX) NOT NULL,
+    [contactid] INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
