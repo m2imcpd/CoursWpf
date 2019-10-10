ef8256dfc9583447d92449cfb1637bde297ec5bc
ihab <m2imcdp@gmail.com>
2019-10-10 15:40:35 +0200

    Correction Hopital Partie 4

---
+++ CoursWPF/GestionRendezVousHopital/Models/Patient.cs
@@ -0,0 +1,101 @@
+﻿using GestionRendezVousHopital.Tools;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionRendezVousHopital.Models
+{
+    public class Patient
+    {
+        private int id;
+        private string nom;
+        private DateTime dateNaissance;
+        private string adresse;
+        private string sexePatient;
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public DateTime DateNaissance { get => dateNaissance; set => dateNaissance = value; }
+        public string Adresse { get => adresse; set => adresse = value; }
+        public string SexePatient { get => sexePatient; set => sexePatient = value; }
+
+        public bool Add()
+        {
+            bool res = false;
+            DataBase.Instance.command = new SqlCommand("INSERT INTO patient (nomPatient, adressePatient, dateNaissance, sexePatient) OUTPUT INSERTED.ID values(@nomPatient, @adressePatient, @dateNaissance, @sexePatient)", DataBase.Instance.connection);
+            DataBase.Instance.command.Parameters.Add(new SqlParameter("@nomPatient", Nom));
+            DataBase.Instance.command.Parameters.Add(new SqlParameter("@adressePatient", Adresse));
+            DataBase.Instance.command.Parameters.Add(new SqlParameter("@dateNaissance", DateNaissance));
+            DataBase.Instance.command.Parameters.Add(new SqlParameter("@sexePatient", SexePatient));
+            DataBase.Instance.connection.Open();
+            Id = (int)DataBase.Instance.command.ExecuteScalar();
+            DataBase.Instance.command.Dispose();
+            DataBase.Instance.connection.Close();
+            if (Id > 0)
+            {
+                res = true;
+            }
+            return res;
+        }
+
+        public bool Update()
+        {
+            bool res = false;
+            DataBase.Instance.command = new SqlCommand("UPDATE patient set " +
+                "nomPatient = @nomPatient, adressePatient = @adressePatient, dateNaissance = @dateNaissance, sexePatient = @sexePatient " +
+                "WHERE id = @id", DataBase.Instance.connection);
+            DataBase.Instance.command.Parameters.Add(new SqlParameter("@nomPatient", Nom));
+            DataBase.Instance.command.Parameters.Add(new SqlParameter("@adressePatient", Adresse));
+            DataBase.Instance.command.Parameters.Add(new SqlParameter("@dateNaissance", DateNaissance));
+            DataBase.Instance.command.Parameters.Add(new SqlParameter("@sexePatient", SexePatient));
+            DataBase.Instance.command.Parameters.Add(new SqlParameter("@id", Id));
+            DataBase.Instance.connection.Open();
+            if (DataBase.Instance.command.ExecuteNonQuery() > 0)
+            {
+                res = true;
+            }
+            DataBase.Instance.command.Dispose();
+            DataBase.Instance.connection.Close();
+            return res;
+        }
+
+        public bool Delete()
+        {
+            bool res = false;
+            DataBase.Instance.command = new SqlCommand("DELETE FROM patient WHERE id = @id", DataBase.Instance.connection);
+            DataBase.Instance.command.Parameters.Add(new SqlParameter("@id", Id));
+            DataBase.Instance.connection.Open();
+            if (DataBase.Instance.command.ExecuteNonQuery() > 0)
+            {
+                res = true;
+            }
+            DataBase.Instance.command.Dispose();
+            DataBase.Instance.connection.Close();
+            return res;
+        }
+
+        public static Patient GetPatientByNom(string nom)
+        {
+            Patient p = null;
+            DataBase.Instance.command = new SqlCommand("SELECT id, adressePatient, dateNaissance, sexePatient from patient where nomPatient = @nomPatient", DataBase.Instance.connection);
+            DataBase.Instance.command.Parameters.Add(new SqlParameter("@nomPatient", nom));
+            DataBase.Instance.connection.Open();
+            DataBase.Instance.reader = DataBase.Instance.command.ExecuteReader();
+            if (DataBase.Instance.reader.Read())
+            {
+                p = new Patient();
+                p.Id = DataBase.Instance.reader.GetInt32(0);
+                p.Nom = nom;
+                p.DateNaissance = DataBase.Instance.reader.GetDateTime(2);
+                p.SexePatient = DataBase.Instance.reader.GetString(3);
+                p.Adresse = DataBase.Instance.reader.GetString(1);     
+            }
+            DataBase.Instance.command.Dispose();
+            DataBase.Instance.connection.Close();
+            return p;
+        }
+    }
+}
---
+++ CoursWPF/GestionRendezVousHopital/ViewModels/GestionPatientViewModel.cs
@@ -0,0 +1,183 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GestionRendezVousHopital.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GestionRendezVousHopital.ViewModels
+{
+    public class GestionPatientViewModel : ViewModelBase
+    {
+        private Patient patient;
+
+        public string Nom
+        {
+            get => patient.Nom;
+            set
+            {
+                patient.Nom = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string Adresse
+        {
+            get => patient.Adresse;
+            set
+            {
+                patient.Adresse = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public DateTime DateNaissance
+        {
+            get => patient.DateNaissance;
+            set
+            {
+                patient.DateNaissance = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool SexeHomme
+        {
+            get => patient.SexePatient == "Homme";
+
+            set
+            {
+                if (value)
+                    patient.SexePatient = "Homme";
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool SexeFemme
+        {
+            get => patient.SexePatient == "Femme";
+
+            set
+            {
+                if (value)
+                    patient.SexePatient = "Femme";
+                RaisePropertyChanged();
+            }
+        }
+
+        public ICommand NewCommand { get; set; }
+
+        public ICommand SearchCommand { get; set; }
+
+        public ICommand AddCommand { get; set; }
+
+        public ICommand EditCommand { get; set; }
+
+        public ICommand DeleteCommand { get; set; }
+
+
+        public GestionPatientViewModel()
+        {
+            patient = new Patient();
+            NewCommand = new RelayCommand(NewPatient);
+            AddCommand = new RelayCommand(AddPatient);
+            EditCommand = new RelayCommand(UpdatePatient);
+            DeleteCommand = new RelayCommand(DeletePatient);
+            SearchCommand = new RelayCommand(SearchPatient);
+        }
+
+        
+        private void NewPatient()
+        {
+            patient = new Patient();
+            Nom = "";
+            Adresse = "";
+            DateNaissance = DateTime.Now;
+            SexeHomme = false;
+            SexeFemme = false;
+        }
+
+
+        private void AddPatient()
+        {
+            if (patient.Add())
+            {
+                MessageBox.Show("Patient ajouté avec numéro : " + patient.Id);
+                NewPatient();
+            }
+            else
+            {
+                MessageBox.Show("Erreur d'insertion");
+            }
+        }
+
+        private void SearchPatient()
+        {
+            patient = Patient.GetPatientByNom(Nom);
+            if (patient == null)
+            {
+                MessageBox.Show("Aucun patient");
+                NewPatient();
+            }
+            else
+            {
+                RaiseAllProperties();
+            }
+        }
+
+        private void UpdatePatient()
+        {
+
+            if (patient.Id > 0)
+            {
+                if (patient.Update())
+                {
+                    MessageBox.Show("patient mis à jour");
+                }
+                else
+                {
+                    MessageBox.Show("Erreur serveur");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Merci de rechercher un patient");
+            }
+        }
+
+        private void DeletePatient()
+        {
+
+            if (patient.Id > 0)
+            {
+                if (patient.Delete())
+                {
+                    MessageBox.Show("patient supprimé");
+                    NewPatient();
+                    RaiseAllProperties();
+                }
+                else
+                {
+                    MessageBox.Show("Erreur serveur");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Merci de rechercher un patient");
+            }
+        }
+
+        private void RaiseAllProperties()
+        {
+            RaisePropertyChanged("Nom");
+            RaisePropertyChanged("DateNaissance");
+            RaisePropertyChanged("Adresse");
+            RaisePropertyChanged("SexeHomme");
+            RaisePropertyChanged("SexeFemme");
+        }
+    }
+}
