8327107d6c3cc632c1156cd5ea7b55301cee8f7a
ihab <m2imcdp@gmail.com>
2019-10-14 12:11:55 +0200

    Correction WPF hopital avec TextBoxMask

---
+++ CoursWPF/GestionRendezVousHopital/Tools/TextBoxMask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace GestionRendezVousHopital.Tools
+{
+    public class TextBoxMask : TextBox
+    {
+
+        public static DependencyProperty NombreMaskProperty = DependencyProperty.Register("NbNombreMask", typeof(int), typeof(TextBoxMask));
+        public int NbNombreMask { get => (int)base.GetValue(NombreMaskProperty); set => base.SetValue(NombreMaskProperty, value); }
+        public TextBoxMask() : base()
+        {
+            DataContext = new Binding("Telephone");
+            Text = "";
+            for (int i = 1; i <= 10; i++)
+            {
+                Text += " * ";
+            }
+            GotFocus += (sender, e) =>
+            {
+                Text = "";
+                
+            };
+        }
+    }
+}
